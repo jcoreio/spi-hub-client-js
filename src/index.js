95c620288805125c586beafec00ea6aff8e9cd27
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-10-29 22:29:57 -0500

    feat: switch to ES6, export Flow Type defs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 // @flow
 
 import assert from 'assert'
+import logger from 'log4jcore'
 import EventEmitter from '@jcoreio/typed-event-emitter'
 
-import {isBuffer, isInteger, isObject, isString} from 'lodash'
 import {MessageClient} from 'socket-ipc'
 import {VError} from 'verror'
+
+const log = logger('spi-hub-client')
 
 const SPI_HUB_SOCKET_PATH = '/tmp/socket-spi-hub'
 
 const IPC_PROTO_VERSION = 1
 
 // Commands that are valid both on the SPI bus and on the IPC socket
-const SPI_HUB_CMD_MSG_TO_DEVICE   = 1
+const SPI_HUB_CMD_MSG_TO_DEVICE = 1
 const SPI_HUB_CMD_MSG_FROM_DEVICE = 2
 // Commands that are only valid on the IPC socket
-const SPI_HUB_CMD_DEVICES_LIST    = 100
+const SPI_HUB_CMD_DEVICES_LIST = 100
 
 const IPC_DEVICE_MESSAGE_OVERHEAD = 7
 
@@ -26,7 +28,7 @@ export type SPIHubClientOpts = {
   binary?: ?boolean,
 }
 
-export type SPIHubClientTxMessage = {
+export type MessageToSPI = {
   busId: number,
   deviceId: number,
   channel: number,
@@ -80,7 +82,7 @@ export default class SPIHubClient extends EventEmitter<SPIHubClientEmittedEvents
     this._ipcClient.start()
   }
 
-  send(txMessage: SPIHubClientTxMessage) {
+  send(txMessage: MessageToSPI) {
     assert(txMessage)
     const {busId, deviceId, channel, deDupeId, message} = txMessage
     const deDupeIdFinal = deDupeId || 0
@@ -89,7 +91,8 @@ export default class SPIHubClient extends EventEmitter<SPIHubClientEmittedEvents
     validateUInt8(channel, 'channel')
     validateUInt16(deDupeIdFinal, 'deDupeId')
 
-    const msgPayloadBuffer: Buffer = isBuffer(message) ? message: Buffer.from(message)
+    const msgPayloadBuffer: Buffer = typeof message === 'string' ? Buffer.from(message) : message
+    assert(Buffer.isBuffer(msgPayloadBuffer))
     const ipcMsgBuf = Buffer.alloc(msgPayloadBuffer.length + IPC_DEVICE_MESSAGE_OVERHEAD)
     let pos = 0
     ipcMsgBuf.writeUInt8(IPC_PROTO_VERSION, pos++)
@@ -104,54 +107,53 @@ export default class SPIHubClient extends EventEmitter<SPIHubClientEmittedEvents
   }
 
   _onIPCMessage(event: {data: Buffer}) {
-    const ipcMsgBuf = event.data;
+    const ipcMsgBuf = event.data
     try {
       assert(ipcMsgBuf.length > 2, 'ipc message is too short')
       let pos = 0
       const version = ipcMsgBuf.readUInt8(pos++)
       const cmd = ipcMsgBuf.readUInt8(pos++)
       assert.strictEqual(version, IPC_PROTO_VERSION, 'unexpected protocol version')
-      switch(cmd) {
-        case SPI_HUB_CMD_DEVICES_LIST: {
-          const strMessage = ipcMsgBuf.toString('utf8', pos)
-          const {devices, serialNumber, accessCode} = JSON.parse(strMessage)
-          this.emit(SPI_HUB_EVENT_DEVICES_CHANGED, {devices, serialNumber, accessCode})
-        } break
-        case SPI_HUB_CMD_MSG_FROM_DEVICE: {
-          assert(ipcMsgBuf.length >= IPC_DEVICE_MESSAGE_OVERHEAD, 'message from device is too short')
-          const busId = ipcMsgBuf.readUInt8(pos++)
-          const deviceId = ipcMsgBuf.readUInt8(pos++)
-          const channel = ipcMsgBuf.readUInt8(pos++)
-          pos += 2 // Throw away the de-dupe ID
-          const messageFromSPI: MessageFromSPI = {busId, deviceId, channel}
-          if (this._binary) {
-            const messageBuffer = messageFromSPI.messageBuffer = Buffer.alloc(ipcMsgBuf.length - pos)
-            ipcMsgBuf.copy(messageBuffer, 0, pos)
-          } else {
-            messageFromSPI.message = ipcMsgBuf.toString('utf8', pos)
-          }
-          this.emit(SPI_HUB_EVENT_MESSAGE, messageFromSPI);
-        } break
-        default:
-          throw Error('unexpected IPC message ID: ' + cmd)
+      switch (cmd) {
+      case SPI_HUB_CMD_DEVICES_LIST: {
+        const strMessage = ipcMsgBuf.toString('utf8', pos)
+        const {devices, serialNumber, accessCode} = JSON.parse(strMessage)
+        this.emit(SPI_HUB_EVENT_DEVICES_CHANGED, {devices, serialNumber, accessCode})
+      } break
+      case SPI_HUB_CMD_MSG_FROM_DEVICE: {
+        assert(ipcMsgBuf.length >= IPC_DEVICE_MESSAGE_OVERHEAD, 'message from device is too short')
+        const busId = ipcMsgBuf.readUInt8(pos++)
+        const deviceId = ipcMsgBuf.readUInt8(pos++)
+        const channel = ipcMsgBuf.readUInt8(pos++)
+        pos += 2 // Throw away the de-dupe ID
+        const messageFromSPI: MessageFromSPI = {busId, deviceId, channel}
+        if (this._binary) {
+          const messageBuffer = messageFromSPI.messageBuffer = Buffer.alloc(ipcMsgBuf.length - pos)
+          ipcMsgBuf.copy(messageBuffer, 0, pos)
+        } else {
+          messageFromSPI.message = ipcMsgBuf.toString('utf8', pos)
+        }
+        this.emit(SPI_HUB_EVENT_MESSAGE, messageFromSPI)
+      } break
+      default:
+        throw Error('unexpected IPC message ID: ' + cmd)
       }
     } catch (err) {
-      console.error('spi-hub-client could not process an incoming IPC message:', err.stack);
+      log.error('could not process an incoming IPC message', err)
     }
   }
 }
 
-function validateUInt8(val, fieldName) {
-  return validateUInt(val, fieldName, 255)
+function validateUInt8(val: number, fieldName: string) {
+  validateUInt(val, fieldName, 255)
 }
 
-function validateUInt16(val, fieldName) {
-  return validateUInt(val, fieldName, 65535)
+function validateUInt16(val: number, fieldName: string) {
+  validateUInt(val, fieldName, 65535)
 }
 
-function validateUInt(val, fieldName, maxValue) {
+function validateUInt(val: number, fieldName: string, maxValue: number) {
   if (!Number.isInteger(val)) throw Error(`${fieldName} must be a valid integer`)
   if (val < 0) throw Error(`${fieldName} must be at least 0`)
   if (val > maxValue) throw Error(`${fieldName} must be ${maxValue} or less`)
-  return val
 }
